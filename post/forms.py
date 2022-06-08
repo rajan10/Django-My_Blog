efdcd19c9ff13167a41e40749e19c68cd81d4ddf
Rajan Gauchan <hirajan10@hotmail.com>
2022-06-08 19:14:00 -0400

    later updated

--- post/forms.py
+++ post/forms.py
@@ -2,15 +2,16 @@ from django import forms
 from .models import Post, Comment
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=['title','image','content']
 
-class PostForm(forms.Form):
-    author=forms.CharField(required=True)
-    title=forms.CharField(required=True)
-    image=forms.ImageField()
-    content=forms.CharField(required=True)
-    created_date=forms.DateTimeField()
+
 
 class CommentForm(forms.Form):
+
+    # model view  content only
     commentor=forms.CharField(required=True)
     post=forms.CharField(required=True)
     content=forms.CharField(required=True)
--- post/views.py
+++ post/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Post, Comment
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     context={}
-    if request.user:
-        # print("logged in user")
-        context['user']=request.user
-        return render(request, template_name='index.html', context=context)
-    else:
-        print("no  user ")
-        return render(request, template_name='index.html',context=context)
+    # if request.user:
+    #     # print("logged in user")
+    #     context['user']=request.user
+    #     return render(request, template_name='index.html', context=context)
+    # else:
+    #     print("no  user ")
+    #     return render(request, template_name='index.html',context=context)
+    posts=Post.objects.all()
+    context['posts']=posts
+    return render(request, template_name="index.html", context=context)
 
 
+
+
+@login_required(login_url='/login')
 def create_post(request):
     if request.method=="GET":
         context={}
@@ -22,22 +29,22 @@ def create_post(request):
     if request.method=="POST":
         form=PostForm(request.POST)
         if form.is_valid():
-            author = form.cleaned_data['author']
+            author = request.user
             title = form.cleaned_data['title']
             image = form.cleaned_data['image']
             content = form.cleaned_data['content']
-            created_date = form.cleaned_data['created_date']
-            post=Post(author=author,title=title,image=image,content=content,created_date=created_date)
+            post=Post(author=author,title=title,image=image,content=content)
             post.save()
             return redirect('index')
         else:
             print("form is invalid")
             return redirect('index')
 
+@login_required(login_url='/login')
 def update_post(request, id):
     post=get_object_or_404(Post, pk=id)
     if request.method=="GET":
-        data={'author':post.author,'title':post.title,'image':post.image,'content':post.content,'created_date':post.created_date}
+        data={'title':post.title,'image':post.image,'content':post.content}
         form=PostForm(data)
         context={}
         context['form']=form
@@ -45,16 +52,17 @@ def update_post(request, id):
     if request.method=="POST":
         form=PostForm(request.POST)
         if form.is_valid():
-            post.author=form.cleaned_data['author']
+            # post.author=form.cleaned_data['author']
             post.title=form.cleaned_data['title']
             post.image=form.cleaned_data['image']
             post.content=form.cleaned_data['content']
-            post.created_date=form.cleaned_data['created_date']
-            post.save(update_fields=['author','title','image','content','created_date'])
+            post.save(update_fields=['title','image','content'])
             return redirect('/')
         else:
             print("form is invalid")
             return redirect('/')
+
+@login_required(login_url='/login')
 def delete_post(request, id):
     post=Post.objects.get(pk=id)
     post.delete()
